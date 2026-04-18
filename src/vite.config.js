39791bcb35091631e33adef91e83d0629ec05100
eddy0612 <jason@edmeades.me.uk>
2026-04-18 13:50:37 +0100

    Try to get github pages working

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   root: '.',
-  base: '/',
+  base: command === 'build' ? '/PlateUpTool/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
@@ -12,4 +12,4 @@ export default defineConfig({
     }
   },
   server: { port: 5173 }
-})
+}))
