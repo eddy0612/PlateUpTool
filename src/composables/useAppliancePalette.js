e61ac6fa565401a256f1f5e865f2a089e96ec315
eddy0612 <157434478+eddy0612@users.noreply.github.com>
2026-04-28 11:49:49 +0100

    Try preloading all 2D images to reduce delays (#11)

--- src/composables/useAppliancePalette.js
+++ src/composables/useAppliancePalette.js
@@ -6,8 +6,19 @@ const palette = ref([])
 const loading = ref(true)
 const error = ref(null)
 
+
 getAppliancePalette()
-  .then(data => { palette.value = data; loading.value = false })
+  .then(data => {
+    palette.value = data;
+    // Preload all 2D images
+    data.forEach(entry => {
+      if (entry.icon2D) {
+        const img = new window.Image();
+        img.src = entry.icon2D;
+      }
+    });
+    loading.value = false;
+  })
   .catch(err => { error.value = err; loading.value = false })
 
 export function useAppliancePalette() {
